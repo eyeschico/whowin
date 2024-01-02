0052d0268e883ddf80898ec30a98701d67993422
Mohammed Khalef <eyeschico@mohammedkhalef.com>
2024-01-02 11:33:39 +0100

    fix: Start View import

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import StartView from "../views/StartView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -12,10 +13,11 @@ const router = createRouter({
     {
       path: "/start",
       name: "start",
+      component: StartView,
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import("../views/StartView.vue"),
+      //component: () => import("../views/StartView.vue"),
     },
   ],
 });
